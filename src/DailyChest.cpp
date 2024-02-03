1b86d50b554ec6a1eb67ea101d663d02006b9acf
Jouca <progameur009@gmail.com>
2024-02-03 10:56:17 +0100

    Fixed crash for daily chest notifications

--- src/DailyChest.cpp
+++ src/DailyChest.cpp
@@ -8,10 +8,19 @@ void DailyChest::getRewards(unsigned int type) {
 };
 
 void DailyChest::rewardsStatusFinished(int p0) {
-    int time1 = -1;
-    int time2 = -1;
-    time1 = GameStatsManager::sharedState()->getRewardItem(GJRewardType::Small);
-    time2 = GameStatsManager::sharedState()->getRewardItem(GJRewardType::Large);
+    GameStatsManager* gsm;
+    // Check if rewards are loaded
+    gsm = GameStatsManager::sharedState();
+    if (gsm->m_rewardItems->count() == 0) return;
+
+    // Get the rewards
+    int time1_value = 0x1;
+    int time2_value = 0x2;
+    GJRewardItem* time1 = dynamic_cast<GJRewardItem*>(gsm->m_rewardItems->objectForKey(time1_value));
+    GJRewardItem* time2 = dynamic_cast<GJRewardItem*>(gsm->m_rewardItems->objectForKey(time2_value));
+
+    geode::log::info("Small Daily Chest: {}", time1);
+    geode::log::info("Big Daily Chest: {}", time2);
 
     sio::message::ptr msg = sio::object_message::create();
     msg->get_map()["sprite"] = sio::string_message::create("GJ_square04-uhd.png");
@@ -25,15 +34,19 @@ void DailyChest::rewardsStatusFinished(int p0) {
     msg->get_map()["verified_coins"] = sio::int_message::create(0);
     msg->get_map()["platformer"] = sio::int_message::create(0);
 
-    if (time1 == 0) {
-        msg->get_map()["type"] = sio::int_message::create(3);
-        msg->get_map()["title"] = sio::string_message::create("Small Daily Chest available!");
-        EventsPush::pushRateLevel(CCDirector::sharedDirector()->getRunningScene(), msg);
+    if (time1 != nullptr) {
+        if (time1->m_timeRemaining == 0) {
+            msg->get_map()["type"] = sio::int_message::create(3);
+            msg->get_map()["title"] = sio::string_message::create("Small Daily Chest available!");
+            EventsPush::pushRateLevel(CCDirector::sharedDirector()->getRunningScene(), msg);
+        }
     }
 
-    if (time2 == 0) {
-        msg->get_map()["type"] = sio::int_message::create(4);
-        msg->get_map()["title"] = sio::string_message::create("Big Daily Chest available!");
-        EventsPush::pushRateLevel(CCDirector::sharedDirector()->getRunningScene(), msg);
+    if (time2 != nullptr) {
+        if (time2->m_timeRemaining == 0) {
+            msg->get_map()["type"] = sio::int_message::create(4);
+            msg->get_map()["title"] = sio::string_message::create("Big Daily Chest available!");
+            EventsPush::pushRateLevel(CCDirector::sharedDirector()->getRunningScene(), msg);
+        }
     }
 };
