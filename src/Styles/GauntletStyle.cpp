bd203cc77bb1cc77c282a9a710ff4d38b693639a
Jouca <progameur009@gmail.com>
2024-05-14 00:44:33 +0900

    Custom gauntlets designs

---
+++ src/Styles/GauntletStyle.cpp
@@ -0,0 +1,60 @@
+#include "../includes.h"
+#include <Geode/modify/GauntletSelectLayer.hpp>
+#include <Geode/Geode.hpp>
+
+class $modify(GauntletSelectLayer) {
+    void setupGauntlets() {
+        GauntletSelectLayer::setupGauntlets();
+
+        auto list = this->getChildByIDRecursive("gauntlets-list");
+
+        log::info("Gauntlet list: {}", list);
+
+        if (list) {
+            CCArray* pages = typeinfo_cast<CCArray*>(list->getChildByID("gauntlet-pages")->getChildren());
+
+            for (int i = 0; i < pages->count(); i++) {
+                auto page = typeinfo_cast<CCMenu*>(typeinfo_cast<CCLayer*>(pages->objectAtIndex(i))->getChildren()->objectAtIndex(0))->getChildren();
+
+                for (int j = 0; j < page->count(); j++) {
+                    GauntletNode* node = typeinfo_cast<GauntletNode*>(typeinfo_cast<CCMenuItemSpriteExtra*>(page->objectAtIndex(j))->getChildByIDRecursive("gauntlet-node"));
+
+                    CCLabelBMFont* label = typeinfo_cast<CCLabelBMFont*>(node->getChildByID("gauntlet-name-label"));
+                    CCLabelBMFont* label_shadow = typeinfo_cast<CCLabelBMFont*>(node->getChildByID("gauntlet-name-shadow-label"));
+                    CCLabelBMFont* label_gauntlet = typeinfo_cast<CCLabelBMFont*>(node->getChildByID("gauntlet-label"));
+                    CCLabelBMFont* label_shadow_gauntlet = typeinfo_cast<CCLabelBMFont*>(node->getChildByID("gauntlet-shadow-label"));
+                    CCScale9Sprite* bg = typeinfo_cast<CCScale9Sprite*>(node->getChildByID("background"));
+
+                    CCNode* gauntlet_node = node->getChildByID("gauntlet-info-node");
+                    CCSprite* gauntlet = typeinfo_cast<CCSprite*>(gauntlet_node->getChildByID("gauntlet-sprite"));
+                    CCSprite* gauntlet_shadow = typeinfo_cast<CCSprite*>(gauntlet_node->getChildByID("gauntlet-shadow-sprite"));
+
+                    bg->setVisible(false);
+                    label->setVisible(false);
+                    label_shadow->setVisible(false);
+                    label_gauntlet->setVisible(false);
+                    label_shadow_gauntlet->setVisible(false);
+                    gauntlet->setVisible(false);
+                    gauntlet_shadow->setVisible(false);
+
+                    std::string gauntletName = label->getString();
+                    if (gauntletName == "World") {
+                        CCSprite* sprite = CCSprite::create(Mod::get()->expandSpriteName("world_gauntlet.png"));
+                        sprite->setPosition(bg->getPosition());
+                        sprite->setScale(1.225f);
+                        sprite->setZOrder(0);
+                        node->addChild(sprite);
+                    } else {
+                        bg->setVisible(true);
+                        label->setVisible(true);
+                        label_shadow->setVisible(true);
+                        label_gauntlet->setVisible(true);
+                        label_shadow_gauntlet->setVisible(true);
+                        gauntlet->setVisible(true);
+                        gauntlet_shadow->setVisible(true);
+                    }
+                }
+            }
+        }
+    }
+};
