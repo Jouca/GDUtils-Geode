b896e2199acaad4f267b120749187906dab351ea
Jouca <progameur009@gmail.com>
2024-06-14 10:50:50 +0900

    2nd fixed _spr

--- src/MoreLeaderboards/MoreLeaderboards.cpp
+++ src/MoreLeaderboards/MoreLeaderboards.cpp
@@ -194,7 +194,7 @@ bool MoreLeaderboards::init(std::string type) {
         // Change region
         auto menu_region = CCMenu::create();
 
-        auto regionSpr = CCSprite::create(Mod::get()->expandSpriteName("earth_btn.png"));
+        auto regionSpr = CCSprite::create(Mod::get()->expandSpriteName("earth_btn.png"_spr));
         regionSpr->setScale(.8f);
         auto regionBtn = CCMenuItemSpriteExtra::create(
             regionSpr,
